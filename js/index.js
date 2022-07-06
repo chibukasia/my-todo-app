e810ec075ffc629afc8a28b425b362d5e3289d78
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-07-07 00:23:12 +0300

    added due date feature

--- js/index.js
+++ js/index.js
@@ -6,16 +6,41 @@ const tbody = document.querySelector('.tbody');
 function addActivity(e){
     e.preventDefault();
     const activity = document.getElementById('todo-id').value;
+    const dueDate = document.getElementById('date').value;
     let tableRow = document.createElement('tr');
     tableRow.classList = "tr";
+    let timeLeft;
+    function activityCountDown(){
+        let currentTime = new Date();
+        let dueDateinMillisec = new Date(dueDate)
+        let timeRemaining = dueDateinMillisec.getTime() - currentTime.getTime();
 
+        //convert remaing time to days, hours, mins and seconds
+        let days = Math.floor(timeRemaining/(1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeRemaining %(1000 * 60 * 60 * 24))/ (1000 * 60 * 60));
+        let minutes = Math.floor((timeRemaining % (1000 * 60 * 60))/(1000 * 60));
+        let seconds = Math.floor((timeRemaining % (1000 * 60))/1000)
+
+        timeLeft = `${days} Days ${hours} Hrs ${minutes} Mins ${seconds} Secs`
+
+        if (timeRemaining < 0){
+            timeLeft = "Out of deadline"
+        }
+        return timeLeft;
+    }
+
+    activityCountDown();
+    
     if (activity.trim()!=""){
         tableRow.innerHTML = `
             <td id="activity">${activity}</td>
+            <td id="due-date">${dueDate}</td>
+            <td id="time-remaining">${timeLeft}</td>
             <td><button type="button" class= "btn" id="edit-btn">Edit</td>
+            <td><button type="button" class= "btn" id="update-btn">Update</td>
             <td><input type="checkbox" id="check">Mark As Done</td>  
             <td><button type="button" class= "btn" id="delete-btn">Delete</td>
-            <td><button type="button" class= "btn" id="update-btn">Update</td>
+            
 
         `
         // delete an activity
@@ -50,6 +75,8 @@ function addActivity(e){
         tbody.appendChild(tableRow)
     }
     form.reset()
+//    setInterval(activityCountDown, 1000)
 }
 
+//setInterval(addActivity, 1000)
 form.addEventListener('submit', addActivity)
